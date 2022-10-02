13158b699658494ed2e30d1952b072474ced83b9
devesh2617 <103035746+devesh2617@users.noreply.github.com>
2022-10-03 02:15:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+//detecting button press
+var n = document.querySelectorAll(".drum").length;
+for (var i = 0; i < n; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var whichButton = this.innerHTML;
+        var currentKey = whichButton;
+        buttonAnimation(whichButton);
+        switch (whichButton) {
+            case "w":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "a":
+                var kickBass = new Audio("sounds/kick-bass.mp3");
+                kickBass.play();
+                break;
+
+            case "s":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+            case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+        }
+    });
+}
+
+//detecting key press
+document.addEventListener("keypress", function (event) {
+    var whichKey = event.key;
+    buttonAnimation(whichKey);
+    switch (whichKey) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "a":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+    }
+});
+
+//adding animation to the keys
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(
+        function () {
+            activeButton.classList.remove("pressed");
+        }, 100
+    );
+}
